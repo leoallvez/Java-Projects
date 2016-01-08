e8460171cd4ad6252959776d9049d8076e392d9a
leoallvez <leoallvez@hotmail.com>
2016-01-08 01:36:17 -0200

    v2

--- TesteDeConversão/src/testedeconversão/TesteDeConversão.java
+++
@@ -1,9 +0,0 @@
-package testedeconversão;
-// Esse programa converte um número inteiro em uma variável char.
-public class TesteDeConversão {
-    public static void main(String[] args) {
-        int x = 1000;
-        char a = (char) x;
-        System.out.print (a);
-    }
-}
